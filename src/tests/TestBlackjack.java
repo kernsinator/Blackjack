404c7f758665f61b15274707609d3c7c63a78eb9
kernsinator <tkerns@protonmail.com>
2020-04-12 13:03:46 -0500

    First commit

--- src/tests/TestBlackjack.java
+++ src/tests/TestBlackjack.java
@@ -1,6 +1,5 @@
 package tests;
 
-import deck.Card;
 import game.Game;
 import players.Player;
 
@@ -9,33 +8,17 @@ public class TestBlackjack {
 
     public static void main(String[] args) {
 
-        //Deck d = new Deck();
-        //d.printDeck();
+
         Player player = new Player();
         Player dealer = new Player(true);
 
-        Card c1, c2;
-
         Game game = new Game(player, dealer);
 
-        //for(int i = 0; i < 500; i++){
+
         game.run();
-        //}
+
         System.out.println("Number of hands you won: " + game.getPlayerWins());
         System.out.println("Number of hands dealer won: " + game.getDealerWins());
-
-
-
-        /*
-        d.shuffleDeck();
-        System.out.println();
-        d.printDeck();
-        for (int i = 0; i <= 10; i++ ){
-            ArrayList<Card> hand = new ArrayList<>();
-            hand.add(d.dealCard());
-            hand.add(d.dealCard());
-            System.out.println(hand);
-            */
 
     }
 }
